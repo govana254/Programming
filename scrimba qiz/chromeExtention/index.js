7e4fe051019a918167e3724834ce99327c6dffa9
GAD KAMAU <gadkamau46@gmail.com>
2023-07-04 23:21:02 +0300

    Added the renderLeads function

--- scrimba qiz/chromeExtention/index.js
+++ scrimba qiz/chromeExtention/index.js
@@ -1,10 +1,17 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
-    console.log(myLeads)
+    renderLeads()
 })
 
-for (i = 0; )
+function renderLeads() {
+    let listItems = ""
+    for (i = 0; i < myLeads.length; i++ ) {
+    listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
+}
