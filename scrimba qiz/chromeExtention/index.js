df0da1870ac59742704a205db530dce06efd82e0
GAD KAMAU <gadkamau46@gmail.com>
2023-07-04 11:29:45 +0300

    First commit

---
+++ scrimba qiz/chromeExtention/index.js
@@ -0,0 +1,8 @@
+let myLeads = []
+const inputEL = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEL.value)
+    console.log(myLeads)
+})
