24ddf99c14ccec62cb8b0eca10833948d8f50cb4
GAD KAMAU <gadkamau46@gmail.com>
2023-07-05 22:57:56 +0300

    Added JSON file and saved on localstorage

--- scrimba qiz/chromeExtention/index.js
+++ scrimba qiz/chromeExtention/index.js
@@ -2,10 +2,12 @@ let myLeads = []
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
     inputEL.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
