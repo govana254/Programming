85c6ce73468419fecd387f374e60b8c282e06922
GAD KAMAU <gadkamau46@gmail.com>
2023-07-05 21:14:48 +0300

    Added some css styling to my ul and a template string

--- scrimba qiz/chromeExtention/index.js
+++ scrimba qiz/chromeExtention/index.js
@@ -1,17 +1,24 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
+    inputEL.value = ""
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
     for (i = 0; i < myLeads.length; i++ ) {
-    listItems += "<li>" + myLeads[i] + "</li>"
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
