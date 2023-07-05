c8bc2cc29f0d946b1a6b49180bab947db5c4024f
GAD KAMAU <gadkamau46@gmail.com>
2023-07-05 23:49:12 +0300

    Added a delete-btn

--- scrimba qiz/chromeExtention/index.js
+++ scrimba qiz/chromeExtention/index.js
@@ -2,7 +2,19 @@ let myLeads = []
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+} )
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
