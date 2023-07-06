cc848f373796914de4173bec4cf4b51622dc4ed7
GAD KAMAU <gadkamau46@gmail.com>
2023-07-06 21:59:00 +0300

    Added a tab-btn

--- scrimba qiz/chromeExtention/index.js
+++ scrimba qiz/chromeExtention/index.js
@@ -4,35 +4,44 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const deleteBtn = document.getElementById("delete-btn")
+const tabbtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-deleteBtn.addEventListener('dblclick', function() {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-} )
+const tabs = [{url: "https://www.linkedin.com/in/per-harald-borgen/"}]
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEL.value)
-    inputEL.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+tabbtn.addEventListener("click", function() {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (i = 0; i < myLeads.length; i++ ) {
+    for (i = 0; i < leads.length; i++ ) {
     listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+} )
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEL.value)
+    inputEL.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
