5772eda22bafd788b977c77581a7f8284b666936
GAD KAMAU <gadkamau46@gmail.com>
2023-07-13 12:30:49 +0300

    Updated database items in realtime using onvalue function

--- scrimba qiz/addToCart mobileApp/index.js
+++ scrimba qiz/addToCart mobileApp/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-7dece-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -23,6 +23,11 @@ addButtonEl.addEventListener("click", function() {
 
     appendItemToShoppingListEl(inputValue)
 
+})
+
+onValue( shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    console.log(itemsArray)
 })
 
 function clearInputFieldEl () {
