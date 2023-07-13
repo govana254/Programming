84a62a48f79ab9397287e8fb954d562323114164
GAD KAMAU <gadkamau46@gmail.com>
2023-07-13 14:26:51 +0300

    Added an eventlistener to newEl that deletes item when clicked

--- scrimba qiz/addToCart mobileApp/index.js
+++ scrimba qiz/addToCart mobileApp/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-7dece-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -51,6 +51,12 @@ function appendItemToShoppingListEl(item) {
     let newEL = document.createElement("li")
 
     newEL.textContent = itemValue
+
+    newEL.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+
+        remove(exactLocationOfItemInDB)
+    })
 
     shoppingListEl.append(newEL)
 }
